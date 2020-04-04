0bb8ecb9f6bec8522ea239a5d38a008f529b6d7f
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2020-04-04 13:51:56 +0200

    Add db context

---
+++ DatingApp/DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Value> Values { get; set; }
+
+        public DataContext([NotNull] DbContextOptions<DataContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
