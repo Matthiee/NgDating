6c4997381c43aab3d857da7c66efe052a14c6979
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2020-04-04 14:06:57 +0200

    Add datacontext migration

---
+++ DatingApp/DatingApp.API/Controllers/ValueController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public ValueController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            return Ok(await this.context.Values.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValues(int id)
+        {
+            return Ok(await this.context.Values.FirstOrDefaultAsync(v => v.Id == id));
+        }
+    }
+}
